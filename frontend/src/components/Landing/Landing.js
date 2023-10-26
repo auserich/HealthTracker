c5ae02d25f71283762dd14aa413adf6b56e43989
Sarah Ortega <snortega1999@gmail.com>
2023-10-26 10:47:12 -0400

    Adding first 3 pages of frontend.

---
+++ frontend/src/components/Landing/Landing.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Landing.css"
+import scale from './scale.png'
+
+const Landing = () => {
+
+
+  return (
+    <div className="landing-container">
+
+      <div className="text-container">
+        <h1>[PH] Health Tracker</h1>
+      </div>
+
+      <div className="landing-buttons">
+        <a href="http://localhost:3000/signup"><button>Signup</button></a>
+        <a href="http://localhost:3000/login"><button style={{marginLeft:'150px'}}>Login</button></a>
+      </div>
+
+      <div className='image-container'>
+        <img src={scale} alt = "scale"/>
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default Landing
--- health-tracker/src/main/java/health_tracker/controller/AuthenticationController.java
+++ health-tracker/src/main/java/health_tracker/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import health_tracker.model.AuthenticationRequest;
 import health_tracker.model.AuthenticationResponse;
 import health_tracker.util.JwtUtil;
 
+@CrossOrigin(allowedHeaders="Access-Control-Allow-Origin")
 @RestController
 public class AuthenticationController {
 	
