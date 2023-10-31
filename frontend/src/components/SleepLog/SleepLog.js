0b69895645ef8aaa165734df16f9f98cceaa9ee1
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 <RSeymourJR04@outlook.com>
2023-10-31 13:27:57 -0400

    pushing Sleep log to Austins waterlog since similar functions

---
+++ frontend/src/components/SleepLog/SleepLog.js
@@ -0,0 +1,143 @@
+import React, { Component, useState } from 'react';
+import { Card, Form, Button } from 'react-bootstrap';
+import WeekDisplay from '../WeekDisplay/WeekDisplay.js';
+import "./SleepLog.css";
+
+const SleepLog = () => {
+	const [sleepMinutes, setSleepMinutes] = useState("");
+	const [sleepDate, setSleepDate] = useState("");
+
+	const [testDate, setTestDate] = useState("");
+
+	const handleMinutesChange = (e) => {
+		setSleepMinutes(e.target.value);
+	};
+
+	const handleDateChange = (e) => {
+		setSleepDate(e.target.value);
+	};
+
+	const handleTestDateChange = (e) => {
+		setTestDate(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const minutes = parseInt(sleepMinutes, 10);
+
+		if (isNaN(minutes)) {
+			console.error("Invalid input for minutes.");
+			return;
+		} else {
+			console.log("Valid Int");
+		}
+
+		const sleepLogData = {
+			minutes: sleepMinutes,
+			date: sleepDate,
+		};
+
+		console.log("ounces: ", waterOunces);
+		console.log("date: ", waterDate);
+
+		fetch("http://localhost:8080/api/sleep", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+			},
+			body: JSON.stringify(sleepLogData),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Data saved:", data); 
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	};
+
+	const userId = 1;
+	// Call the function with the selected date and userId
+	const handleRetrieveWeekLogs = () => {
+		const formattedDate = new Date(testDate).toISOString().split("T")[0];
+		fetchSleepLogsForWeek(formattedDate, userId); // Replace 'userId' with the actual user ID
+	};
+
+	const fetchSleepLogsForWeek = (startDate, userId) => {
+		// const url = `http://localhost:8080/api/sleep/${startDate}/${userId}`;
+		const url = `http://localhost:8080/api/sleep/${userId}`;
+
+		console.log("startDate: ", startDate);
+		console.log("userId: ", userId);
+
+		fetch(url, {
+			method: "GET",
+			headers: {
+				Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Weekly Sleep logs:", data);
+				// Update your state or do something with the data
+			})
+			.catch((error) => {
+				console.error("Error fetching water logs:", error);
+			});
+	};
+
+	return (
+		<>
+			<div className="centered-container">
+				<Card className="sleep-log">
+					<Card.Title>Sleep Log</Card.Title>
+					<Form onSubmit={handleSubmit}>
+						<Form.Group className="mb-3">
+							<Form.Label>Minutes of Sleep</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="Enter minutes"
+								value={sleepMinutes}
+								onChange={handleMinutesChange}
+							/>
+						</Form.Group>
+						<Form.Group className="mb-3">
+							<Form.Label>Date</Form.Label>
+							<Form.Control
+								type="date"
+								placeholder="Enter date"
+								value={sleepDate}
+								onChange={handleDateChange}
+							/>
+						</Form.Group>
+                        
+						<Button variant="primary" type="submit">
+							Submit
+						</Button>
+					</Form>
+				</Card>
+			</div>
+			{/* <WeekDisplay selectedDate={new Date(2023, 9, 28)} /> */}
+			<WeekDisplay selectedDate={new Date()} />
+
+			<Form>
+				<Form.Group className="mb-3">
+					<Form.Label>Test</Form.Label>
+					<Form.Control
+						type="date"
+						placeholder="Enter date"
+						value={testDate}
+						onChange={handleTestDateChange}
+					/>
+				</Form.Group>
+				<Button variant="primary" onClick={handleRetrieveWeekLogs}>
+					Test
+				</Button>
+			</Form>
+		</>
+	);
+};
+
+export default SleepLog;
