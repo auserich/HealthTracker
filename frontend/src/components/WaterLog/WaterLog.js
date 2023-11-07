594e658742aacf09ac3044e92ce319621983c199
Austin <auserich@gmail.com>
2023-11-07 12:23:17 -0800

    finished styles and added some more logic

--- frontend/src/components/WaterLog/WaterLog.js
+++ frontend/src/components/WaterLog/WaterLog.js
@@ -1,24 +1,30 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Card, Form, Button } from "react-bootstrap";
 import WeekDisplay from "../WeekDisplay/WeekDisplay.js";
 import "./WaterLog.css";
 
-const WaterLog = () => {
-	const [waterOunces, setWaterOunces] = useState("");
-	const [waterDate, setWaterDate] = useState("");
+const WaterLog = (props) => {
+	const [waterOunces, setOunces] = useState("");
+	const [waterDate, setDate] = useState("");
+	const [waterId, setId] = useState("");
 
-	const [testDate, setTestDate] = useState("");
+	useEffect(() => {
+		if (props.editMode && props.waterData) {
+			if (props.waterData.id) {
+				setId(props.waterData.id);
+			}
+			if (props.waterData.ounces) {
+				setOunces(props.waterData.ounces);
+			}
+		}
+	}, [props.editMode, props.waterData]);
 
 	const handleOuncesChange = (e) => {
-		setWaterOunces(e.target.value);
+		setOunces(e.target.value);
 	};
 
 	const handleDateChange = (e) => {
-		setWaterDate(e.target.value);
-	};
-
-	const handleTestDateChange = (e) => {
-		setTestDate(e.target.value);
+		setDate(e.target.value);
 	};
 
 	const handleSubmit = (e) => {
@@ -31,90 +37,92 @@ const WaterLog = () => {
 			return;
 		}
 
-		const waterLogData = {
+		const waterData = {
+			id: waterId,
 			ounces: waterOunces,
 			date: waterDate,
 		};
 
-		console.log("ounces: ", waterOunces);
-		console.log("date: ", waterDate);
+		console.log("Water Data: ", waterData);
 
+		if (props.editMode) {
+			// If in "edit" mode, update the water data
+			console.log("UPDATING: ", waterData);
+			editWaterLog(waterData);
+		} else {
+			// Default to POST request
+			console.log("ADDING: ", waterData);
+			addWaterLog(waterData);
+		}
+	};
+
+	const addWaterLog = (waterData) => {
 		fetch("http://localhost:8080/api/water", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 				Authorization: "Bearer " + localStorage.getItem("jwtToken"),
 			},
-			body: JSON.stringify(waterLogData),
+			body: JSON.stringify(waterData),
 		})
 			.then((response) => response.json())
 			.then((data) => {
 				console.log("Data saved: ", data);
+				props.handleClose();
 			})
 			.catch((error) => {
 				console.error("Error: ", error);
 			});
 	};
 
-	const userId = 1;
-	// Call the function with the selected date and userId
-	const handleRetrieveWeekLogs = () => {
-		const formattedDate = new Date(testDate).toISOString().split("T")[0];
-		fetchWaterLogsForWeek(formattedDate, userId); // Replace 'userId' with the actual user ID
-	};
-
-	const fetchWaterLogsForWeek = (startDate, userId) => {
-		const url = `http://localhost:8080/api/water/${userId}`;
-
-		console.log("startDate: ", startDate);
-		console.log("userId: ", userId);
-
-		fetch(url, {
-			method: "GET",
+	const editWaterLog = (waterData) => {
+		fetch("http://localhost:8080/api/water", {
+			method: "PUT",
 			headers: {
+				"Content-Type": "application/json",
 				Authorization: "Bearer " + localStorage.getItem("jwtToken"),
 			},
+			body: JSON.stringify(waterData),
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				console.log("Water logs for the week:", data);
-				// Update your state or do something with the data
+				console.log("Water data updated: ", data);
+				props.handleClose(); // close the modal after successful update
 			})
 			.catch((error) => {
-				console.error("Error fetching water logs:", error);
+				console.error("Error: ", error);
 			});
 	};
 
 	return (
 		<>
-			<div className="centered-container">
-				<Card className="log-card">
-					<Card.Title>Water Log</Card.Title>
-					<Form onSubmit={handleSubmit}>
-						<Form.Group className="mb-3">
-							<Form.Label>Ounces of Water</Form.Label>
-							<Form.Control
-								type="number"
-								placeholder="Enter ounces"
-								value={waterOunces}
-								onChange={handleOuncesChange}
-							/>
-						</Form.Group>
-						<Form.Group className="mb-3">
-							<Form.Label>Date</Form.Label>
-							<Form.Control
-								type="date"
-								placeholder="Enter date"
-								value={waterDate}
-								onChange={handleDateChange}
-							/>
-						</Form.Group>
-						<Button variant="primary" type="submit">
-							Submit
-						</Button>
-					</Form>
-				</Card>
-			</div>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className="mb-3">
+					<Form.Label>Ounces of Water</Form.Label>
+					<Form.Control
+						type="number"
+						placeholder="Enter ounces"
+						value={waterOunces}
+						onChange={handleOuncesChange}
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3">
+					<Form.Label>Date</Form.Label>
+					<Form.Control
+						type="date"
+						placeholder="Enter date"
+						value={waterDate}
+						onChange={handleDateChange}
+					/>
+				</Form.Group>
+				<Button
+					variant="primary"
+					type="submit"
+					className="mx-auto d-block"
+				>
+					{props.editMode ? "Update" : "Submit"}
+				</Button>
+			</Form>
 		</>
 	);
 };
