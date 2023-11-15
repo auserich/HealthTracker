64bd47666b436f625094632b269e1f804ad75089
Austin <auserich@gmail.com>
2023-11-15 06:32:30 -0800

    Added caloriesburned API to backend

--- health-tracker/src/main/java/health_tracker/controller/ExerciseController.java
+++ health-tracker/src/main/java/health_tracker/controller/ExerciseController.java
@@ -3,6 +3,10 @@ package health_tracker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,10 +18,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import health_tracker.exception.ResourceNotFoundException;
 import health_tracker.model.Exercise;
-import health_tracker.model.Meal;
 import health_tracker.model.User;
 import health_tracker.service.ExerciseService;
 import health_tracker.service.UserService;
@@ -26,11 +30,17 @@ import health_tracker.service.UserService;
 @RequestMapping("/api")
 public class ExerciseController {
 
+	@Value("${exercise.api.key}")
+	private String API_KEY;
+	
     @Autowired
     ExerciseService service;
 
 	@Autowired
     UserService userService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
     @GetMapping("/exercise")
 	public List<Exercise> getAllExercises() {
@@ -47,6 +57,20 @@ public class ExerciseController {
 	public ResponseEntity<?> getAllUserExercisesFromDate(@PathVariable int userId, @PathVariable String date) {
 		List<Exercise> found = service.getAllUserExercisesFromDate(userId, date);
 		return ResponseEntity.status(200).body(found);
+	}
+	
+	@GetMapping("exercise/caloriesburned/{exercise}")
+	public ResponseEntity<?> getCaloriesburned(@PathVariable String exercise) {
+		String apiUrl = "https://api.api-ninjas.com/v1/caloriesburned?activity=" + exercise;
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.set("X-Api-Key", API_KEY);
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+		
+		ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+		String responseBody = responseEntity.getBody();
+		
+		return ResponseEntity.status(responseEntity.getStatusCode()).body(responseBody);
 	}
 	
 	@PostMapping("/exercise")
